d547f24f043c50bfbfc94fbaa6b9657cfbc25901
Chenxi Han <5656hcx@gmail.com>
2021-03-06 22:21:22 -0800

    First search page draft hw6

--- hw6/static/script.js
+++ hw6/static/script.js
@@ -5,8 +5,8 @@ function homepage() {
 	home.style.borderBottom = "1px solid white"
 	search.style.color = "white"
 	search.style.borderBottom = "0px"
-	document.getElementById("home_content").style.display="block";
-	document.getElementById("search_content").style.display="none";
+	document.getElementById("home_content").style.display="block"
+	document.getElementById("search_content").style.display="none"
 }
 
 function searchpage() {
@@ -16,6 +16,28 @@ function searchpage() {
 	home.style.borderBottom = "0px"
 	search.style.color = "#990000"
 	search.style.borderBottom = "1px solid white"
-	document.getElementById("home_content").style.display="none";
-	document.getElementById("search_content").style.display="block";
+	document.getElementById("home_content").style.display="none"
+	document.getElementById("search_content").style.display="block"
 }
+
+function validateForm() {
+	var searchbox = document.forms["searchbox"]
+	if (searchbox["keyword"].value == "" || searchbox["category"].value == "") {
+		alert("Please enter valid values.")
+	}
+}
+
+function clearForm() {
+	var searchbox = document.forms["searchbox"]
+	searchbox["keyword"].value = ""
+	searchbox["category"].value = ""
+}
+
+function resizeFooter() {
+	var h = (window.innerHeight - 140) + 'px'
+	document.getElementById("home_content").style.minHeight = h
+	document.getElementById("search_content").style.minHeight = h
+}
+
+window.onload = resizeFooter;
+window.onresize = resizeFooter;
