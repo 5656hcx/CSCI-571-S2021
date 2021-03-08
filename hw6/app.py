df27970e3a8d388bd976bc47e36f2e59e8bc2fd2
Chenxi Han <5656hcx@gmail.com>
2021-03-07 16:47:38 -0800

    Add responsive layout for hw6

--- hw6/app.py
+++ hw6/app.py
@@ -1,9 +1,52 @@
+import json
+from urllib import request
 from flask import Flask, render_template
+
+trending_url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=67124d57621f4b2c70bb13cb9b38bacd'
+airing_url = 'https://api.themoviedb.org/3/tv/airing_today?api_key=67124d57621f4b2c70bb13cb9b38bacd'
+
+count = 5
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-   return render_template("index.html")
+	return render_template("index.html")
+
+@app.route("/trending-and-airing/")
+def fetch_trending_and_airing():
+	results = {}
+	results['trending'] = json.loads(fetch_trending_movies())
+	results['airing'] = json.loads(fetch_airing_today())
+	return json.dumps(results)
+
+@app.route("/trending/")
+def fetch_trending_movies():
+	response = request.urlopen("https://api.themoviedb.org/3/trending/movie/week?api_key=67124d57621f4b2c70bb13cb9b38bacd")
+	text = json.loads(response.read())
+	results = []
+	for i in range(0, count):
+		obj = {
+			'title': text['results'][i]['title'],
+			'release_date': text['results'][i]['release_date'],
+			'backdrop_path': text['results'][i]['backdrop_path']
+		}
+		results.append(obj)
+	return json.dumps(results)
+
+@app.route("/airing/")
+def fetch_airing_today():
+	response = request.urlopen("https://api.themoviedb.org/3/tv/airing_today?api_key=67124d57621f4b2c70bb13cb9b38bacd")
+	text = json.loads(response.read())
+	results = []
+	for i in range(0, count):
+		obj = {
+			'name': text['results'][i]['name'],
+			'first_air_date': text['results'][i]['first_air_date'],
+			'backdrop_path': text['results'][i]['backdrop_path']
+		}
+		results.append(obj)
+	return json.dumps(results)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+	app.run(debug = True)
--- hw6/static/script.js
+++ hw6/static/script.js
@@ -1,3 +1,48 @@
+var elem_trending_img
+var elem_trending_text
+var elem_tvshows_img
+var elem_tvshows_text
+var elem_home_content
+var elem_search_content
+
+function fetchElements()
+{
+	elem_trending_img = document.getElementById("trending_img")
+	elem_trending_text = document.getElementById("trending_text")
+	elem_tvshows_img = document.getElementById("tvshows_img")
+	elem_tvshows_text = document.getElementById("tvshows_text")
+	elem_home_content = document.getElementById("home_content")
+	elem_search_content = document.getElementById("search_content")
+}
+
+function fetchHomeInfo() {
+	fetchElements();
+	resizeFooter();
+	const url = "http://127.0.0.1:5000/trending-and-airing";
+	var xhr = new XMLHttpRequest();
+	index = 0;
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			objs = JSON.parse(this.responseText);
+			elem_trending_img.style.backgroundImage = "url(" + "https://image.tmdb.org/t/p/w780" + objs.trending[index].backdrop_path + ")";
+			elem_trending_text.innerHTML = objs.trending[index].title + " (" + objs.trending[index].release_date.substr(0, 4) + ")";
+			elem_tvshows_img.style.backgroundImage = "url(" + "https://image.tmdb.org/t/p/w780" + objs.airing[index].backdrop_path + ")";
+			elem_tvshows_text.innerHTML = objs.airing[index].name + " (" + objs.airing[index].first_air_date.substr(0, 4) + ")";
+		}
+	};
+	xhr.open("GET", url, true);
+	xhr.send();
+	setInterval(function() {
+		index = index + 1;
+		index_a = index % objs.trending.length;
+		index_b = index % objs.airing.length;
+		elem_trending_img.style.backgroundImage = "url(" + "https://image.tmdb.org/t/p/w780" + objs.trending[index_a].backdrop_path + ")";
+		elem_trending_text.innerHTML = objs.trending[index_a].title + " (" + objs.trending[index_a].release_date.substr(0, 4) + ")";
+		elem_tvshows_img.style.backgroundImage = "url(" + "https://image.tmdb.org/t/p/w780" + objs.airing[index_b].backdrop_path + ")";
+		elem_tvshows_text.innerHTML = objs.airing[index_b].name + " (" + objs.airing[index_b].first_air_date.substr(0, 4) + ")";
+	}, 4000);
+}
+
 function homepage() {
 	var home = document.getElementById("home_tab")
 	var search = document.getElementById("search_tab")
@@ -5,8 +50,8 @@ function homepage() {
 	home.style.borderBottom = "1px solid white"
 	search.style.color = "white"
 	search.style.borderBottom = "0px"
-	document.getElementById("home_content").style.display="block"
-	document.getElementById("search_content").style.display="none"
+	elem_home_content.style.display="block"
+	elem_search_content.style.display="none"
 }
 
 function searchpage() {
@@ -16,8 +61,8 @@ function searchpage() {
 	home.style.borderBottom = "0px"
 	search.style.color = "#990000"
 	search.style.borderBottom = "1px solid white"
-	document.getElementById("home_content").style.display="none"
-	document.getElementById("search_content").style.display="block"
+	elem_home_content.style.display="none"
+	elem_search_content.style.display="block"
 }
 
 function validateForm() {
@@ -35,9 +80,6 @@ function clearForm() {
 
 function resizeFooter() {
 	var h = (window.innerHeight - 140) + 'px'
-	document.getElementById("home_content").style.minHeight = h
-	document.getElementById("search_content").style.minHeight = h
+	elem_home_content.style.minHeight = h
+	elem_search_content.style.minHeight = h
 }
-
-window.onload = resizeFooter;
-window.onresize = resizeFooter;
