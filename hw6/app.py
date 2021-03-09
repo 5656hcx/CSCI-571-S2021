791b385b851a594477581246845761bef0309372
Chenxi Han <5656hcx@gmail.com>
2021-03-08 23:35:13 -0800

    Update search page hw6

--- hw6/app.py
+++ hw6/app.py
@@ -4,10 +4,13 @@ from flask import Flask, render_template
 from flask import request as flask_request
 
 api_key = '67124d57621f4b2c70bb13cb9b38bacd'
+
 trending_endpoint = 'https://api.themoviedb.org/3/trending/movie/week?api_key=<<api_key>>'
 tv_airing_today_endpoint = 'https://api.themoviedb.org/3/tv/airing_today?api_key=<<api_key>>'
 search_endpoint = 'https://api.themoviedb.org/3/search/<<category>>?api_key=<<api_key>>&language=en-US&query=<<search_query>>&page=1&include_adult=false';
+genre_endpoint = 'https://api.themoviedb.org/3/genre/<<category>>/list?api_key=<<api_key>>&language=en-US'
 
+genre_dict = {}
 app = Flask(__name__)
 
 @app.route("/")
@@ -58,6 +61,7 @@ def search_tmdb():
 	url = url.replace('<<category>>', category)
 	url = url.replace('<<search_query>>', keyword.replace(' ', '%20'))
 
+	update_genre()
 	if category == 'movie':
 		return search_movie(url)
 	elif category == 'tv':
@@ -81,7 +85,7 @@ def search_movie(url):
 			'release_date': obj['release_date'],
 			'vote_average': obj['vote_average'],
 			'vote_count': obj['vote_count'],
-			'genre_ids': obj['genre_ids'],}
+			'genre_strs': convert_genre('movie', obj['genre_ids'])}
 		)
 	return json.dumps(results)
 
@@ -94,13 +98,13 @@ def search_tv(url):
 		obj = text['results'][i]
 		results.append({
 			'id': obj['id'],
-			'name': obj['name'],
+			'title': obj['name'],
 			'overview': obj['overview'],
 			'poster_path': obj['poster_path'],
-			'first_air_date': obj['first_air_date'],
+			'release_date': obj['first_air_date'],
 			'vote_average': obj['vote_average'],
 			'vote_count': obj['vote_count'],
-			'genre_ids': obj['genre_ids'],}
+			'genre_strs': convert_genre('tv', obj['genre_ids'])}
 		)
 	return json.dumps(results)
 
@@ -120,20 +124,45 @@ def search_multi(url):
 				'release_date': obj['release_date'],
 				'vote_average': obj['vote_average'],
 				'vote_count': obj['vote_count'],
-				'genre_ids': obj['genre_ids'],}
+				'genre_strs': convert_genre('movie', obj['genre_ids'])}
 			)
 		elif obj['media_type'] == 'tv':
 			results.append({
 				'id': obj['id'],
-				'name': obj['name'],
+				'title': obj['name'],
 				'overview': obj['overview'],
 				'poster_path': obj['poster_path'],
-				'first_air_date': obj['first_air_date'],
+				'release_date': obj['first_air_date'],
 				'vote_average': obj['vote_average'],
 				'vote_count': obj['vote_count'],
-				'genre_ids': obj['genre_ids'],}
+				'genre_strs': convert_genre('tv', obj['genre_ids'])}
 			)
 	return json.dumps(results)
+
+
+def update_genre():
+	global genre_dict
+	if 'movie' not in genre_dict:
+		response = request.urlopen(genre_endpoint.replace('<<category>>', 'movie').replace('<<api_key>>', api_key))
+		text = json.loads(response.read())
+		genre_dict['movie'] = {}
+		for i in range(0, len(text['genres'])):
+			genre_dict['movie'][text['genres'][i]['id']] = text['genres'][i]['name']
+	if 'tv' not in genre_dict:
+		response = request.urlopen(genre_endpoint.replace('<<category>>', 'tv').replace('<<api_key>>', api_key))
+		text = json.loads(response.read())
+		genre_dict['tv'] = {}
+		for i in range(0, len(text['genres'])):
+			genre_dict['tv'][text['genres'][i]['id']] = text['genres'][i]['name']
+
+
+def convert_genre(category, ids):
+	results = []
+	if category not in genre_dict:
+		return ['Unknown']
+	for i in range(0, len(ids)):
+		results.append(genre_dict[category][ids[i]])
+	return results
 
 
 if __name__ == '__main__':
--- hw6/static/script.js
+++ hw6/static/script.js
@@ -7,6 +7,22 @@ var elem_search_content
 var elem_home_tab
 var elem_search_tab
 
+var div_no_results
+var div_show_results
+
+var template_list_item
+
+var main_page_objs
+var main_page_index
+
+function __init_site__()
+{
+	main_page_index = 0
+	fetchElements()
+	resizeFooter()
+	fetchHomeInfo()
+}
+
 function fetchElements()
 {
 	elem_trending_img = document.getElementById("trending_img")
@@ -17,34 +33,40 @@ function fetchElements()
 	elem_search_content = document.getElementById("search_content")
 	elem_home_tab = document.getElementById("home_tab")
 	elem_search_tab = document.getElementById("search_tab")
+
+	div_no_results = document.getElementById("no_results")
+	div_show_results = document.getElementById("show_results")
+
+	template_list_item = document.getElementById("template_list_item")
+}
+
+function loopDisplay() {
+	if (main_page_objs != null) {
+
+		if (main_page_objs.trending[main_page_index].backdrop_path == null) elem_trending_img.style.backgroundImage = "url({{ url_for('static', filename='movie-placeholder.jpg')"
+		else elem_trending_img.style.backgroundImage = "url(" + "https://image.tmdb.org/t/p/w780" + main_page_objs.trending[main_page_index].backdrop_path + ")";
+
+		if (main_page_objs.airing[main_page_index].backdrop_path == null) elem_tvshows_img.style.backgroundImage = "url({{ url_for('static', filename='movie-placeholder.jpg')"
+		else elem_tvshows_img.style.backgroundImage = "url(" + "https://image.tmdb.org/t/p/w780" + main_page_objs.airing[main_page_index].backdrop_path + ")";
+
+		elem_trending_text.innerHTML = main_page_objs.trending[main_page_index].title + " (" + main_page_objs.trending[main_page_index].release_date.substr(0, 4) + ")";
+		elem_tvshows_text.innerHTML = main_page_objs.airing[main_page_index].name + " (" + main_page_objs.airing[main_page_index].first_air_date.substr(0, 4) + ")";
+		main_page_index = (main_page_index + 1) % 5;
+	}
 }
 
 function fetchHomeInfo() {
-	fetchElements();
-	resizeFooter();
 	const url = "http://127.0.0.1:5000/trending-and-airing";
 	var xhr = new XMLHttpRequest();
-	index = 0;
 	xhr.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			objs = JSON.parse(this.responseText);
-			elem_trending_img.style.backgroundImage = "url(" + "https://image.tmdb.org/t/p/w780" + objs.trending[index].backdrop_path + ")";
-			elem_trending_text.innerHTML = objs.trending[index].title + " (" + objs.trending[index].release_date.substr(0, 4) + ")";
-			elem_tvshows_img.style.backgroundImage = "url(" + "https://image.tmdb.org/t/p/w780" + objs.airing[index].backdrop_path + ")";
-			elem_tvshows_text.innerHTML = objs.airing[index].name + " (" + objs.airing[index].first_air_date.substr(0, 4) + ")";
+			main_page_objs = JSON.parse(this.responseText);
+			loopDisplay();
 		}
 	};
 	xhr.open("GET", url, true);
 	xhr.send();
-	setInterval(function() {
-		index = index + 1;
-		index_a = index % objs.trending.length;
-		index_b = index % objs.airing.length;
-		elem_trending_img.style.backgroundImage = "url(" + "https://image.tmdb.org/t/p/w780" + objs.trending[index_a].backdrop_path + ")";
-		elem_trending_text.innerHTML = objs.trending[index_a].title + " (" + objs.trending[index_a].release_date.substr(0, 4) + ")";
-		elem_tvshows_img.style.backgroundImage = "url(" + "https://image.tmdb.org/t/p/w780" + objs.airing[index_b].backdrop_path + ")";
-		elem_tvshows_text.innerHTML = objs.airing[index_b].name + " (" + objs.airing[index_b].first_air_date.substr(0, 4) + ")";
-	}, 4000);
+	setInterval(loopDisplay, 4000);
 }
 
 function switchTab(index) {
@@ -81,9 +103,56 @@ function search() {
 			if (this.readyState == 4 && this.status == 200) {
 				objs = JSON.parse(this.responseText);
 				if (objs.length > 0) {
-					document.getElementById("no_results").style.display="none";
+
+					// dynamic create div
+					var current_count = div_show_results.childElementCount - 2;
+					for (var i = current_count; i < objs.length; i++) {
+						var node = template_list_item.cloneNode(true);
+						node.id = "list_item";
+						div_show_results.appendChild(node);
+					}
+
+					// fill into div
+					var posters = document.getElementsByClassName("list_poster");
+					var titles = document.getElementsByClassName("list_title");
+					var genres = document.getElementsByClassName("list_genre");
+					var years = document.getElementsByClassName("list_year");
+					var ranks = document.getElementsByClassName("list_rank");
+					var votes = document.getElementsByClassName("list_vote");
+					var overviews = document.getElementsByClassName("list_overview");
+
+					for (var i = 0; i < objs.length; i++) {
+
+						var genres_string = '';
+						if (objs[i].genre_strs != null) {
+							for (var j = 0; j < objs[i].genre_strs.length; j++) {
+								genres_string = genres_string + objs[i].genre_strs[j] + ', ';
+							}
+						}
+						genres_string = genres_string == '' ? genres[0].innerHTML : genres_string.substr(0, genres_string.length-2);
+						genres[i+1].innerHTML = genres_string
+
+						posters[i+1].src = objs[i].poster_path ? "https://image.tmdb.org/t/p/w185" + objs[i].poster_path : posters[0].src;
+						titles[i+1].innerHTML = objs[i].title ? objs[i].title : titles[0].innerHTML;
+						years[i+1].innerHTML = objs[i].release_date ? objs[i].release_date.substr(0, 4) : years[0].innerHTML;
+						ranks[i+1].innerHTML = objs[i].vote_average ? "&#9733;&nbsp;" + objs[i].vote_average/2 + "/5" : ranks[0].innerHTML;
+						votes[i+1].innerHTML = objs[i].vote_count ? objs[i].vote_count + " votes" : votes[0].innerHTML;
+						overviews[i+1].innerHTML = objs[i].overview ? objs[i].overview : overviews[0].innerHTML;
+						div_show_results.children[i+2].style.display="block";
+					}
+
+					// hide redundant div
+					for (var i = objs.length; i < current_count; i++) {
+						div_show_results.children[i+2].style.display="none";
+					}
+
+					div_no_results.style.display="none";
+					div_show_results.style.display="block";
 				}
-				else document.getElementById("no_results").style.display="block";
+				else {
+					div_no_results.style.display="block";
+					div_show_results.style.display="none";
+				}
 			}
 		};
 		xhr.open("GET", proxy, true);
@@ -91,14 +160,17 @@ function search() {
 	}
 }
 
-function clear() {
+function desearch() {
 	var searchbox = document.forms["searchbox"]
 	searchbox["keyword"].value = ""
 	searchbox["category"].value = ""
+	div_no_results.style.display="none";
+	div_show_results.style.display="none";
+
 }
 
 function resizeFooter() {
-	var h = (window.innerHeight - 155) + 'px'
+	var h = (window.innerHeight - 164) + 'px'
 	elem_home_content.style.minHeight = h
 	elem_search_content.style.minHeight = h
 }
